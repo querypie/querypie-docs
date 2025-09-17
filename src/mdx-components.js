819da675752c902865818778fbfc42e2d2d1a564
JK <jk@chequer.io>
2025-09-17 13:35:53 +0900

    Fix by Cursor

--- src/mdx-components.js
+++ src/mdx-components.js
@@ -1,8 +1,61 @@
-import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs';
+// Import the custom layout component
+import CustomTocLayout from '@/components/custom-toc-layout';
 
-const docsComponents = getDocsMDXComponents();
+// Use only basic HTML elements for MDX components to avoid SSR issues
+export const useMDXComponents = components => {
+  // Filter out undefined components
+  const filteredComponents = {};
+  if (components) {
+    Object.keys(components).forEach(key => {
+      if (components[key] !== undefined) {
+        filteredComponents[key] = components[key];
+      }
+    });
+  }
 
-export const useMDXComponents = components => ({
-  ...docsComponents,
-  ...components,
-});
+  return {
+    // Use standard HTML elements only
+    a: 'a',
+    button: 'button',
+    details: 'details',
+    summary: 'summary',
+    div: 'div',
+    span: 'span',
+    p: 'p',
+    h1: 'h1',
+    h2: 'h2',
+    h3: 'h3',
+    h4: 'h4',
+    h5: 'h5',
+    h6: 'h6',
+    ul: 'ul',
+    ol: 'ol',
+    li: 'li',
+    img: 'img',
+    code: 'code',
+    pre: 'pre',
+    blockquote: 'blockquote',
+    table: 'table',
+    thead: 'thead',
+    tbody: 'tbody',
+    tr: 'tr',
+    th: 'th',
+    td: 'td',
+    strong: 'strong',
+    em: 'em',
+    br: 'br',
+    hr: 'hr',
+    section: 'section',
+    article: 'article',
+    header: 'header',
+    footer: 'footer',
+    nav: 'nav',
+    aside: 'aside',
+    main: 'main',
+    figure: 'figure',
+    figcaption: 'figcaption',
+    // Add wrapper component
+    wrapper: CustomTocLayout,
+    ...filteredComponents,
+  };
+};
