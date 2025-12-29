6b89518ac454ea4f2e09654250f266885842527f
JK <jk@chequer.io>
2025-12-29 21:31:21 +0900

    src/content/ko/api-reference/_meta.ts

--- src/content/ko/api-reference/_meta.ts
+++ src/content/ko/api-reference/_meta.ts
@@ -1,7 +1,17 @@
 export default {
   '11.4.1': {
-    title: '버전 11.4.1',
-    type: 'page',
+    type: 'menu',
+    title: '11.4.1',
+    items: {
+      v2: {
+        title: 'v2',
+        href: '/api-reference/11.4.1/v2',
+      },
+      'v0.9': {
+        title: 'v0.9',
+        href: '/api-reference/11.4.1/v0.9',
+      },
+    },
   },
 };
 
