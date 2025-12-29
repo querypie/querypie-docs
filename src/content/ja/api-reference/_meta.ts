0765472fd95502103d75e94b82382d3f9bff5e8f
JK <jk@chequer.io>
2025-12-29 21:45:55 +0900

    {en,ja,ko}/api-reference/_meta.ts

--- src/content/ja/api-reference/_meta.ts
+++ src/content/ja/api-reference/_meta.ts
@@ -1,7 +1,21 @@
-export default {
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
   '11.4.1': {
-    title: 'バージョン 11.4.1',
-    type: 'page',
+    type: 'menu',
+    title: '11.4.1',
+    items: {
+      v2: {
+        title: 'v2',
+        href: '11.4.1/v2',
+      },
+      'v0.9': {
+        title: 'v0.9',
+        href: '11.4.1/v0.9',
+      },
+    },
   },
 };
+
+export default meta;
 
--- src/content/ko/api-reference/_meta.ts
+++ src/content/ko/api-reference/_meta.ts
@@ -1,17 +1,21 @@
-export default {
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
   '11.4.1': {
     type: 'menu',
     title: '11.4.1',
     items: {
       v2: {
         title: 'v2',
-        href: '/api-reference/11.4.1/v2',
+        href: '11.4.1/v2',
       },
       'v0.9': {
         title: 'v0.9',
-        href: '/api-reference/11.4.1/v0.9',
+        href: '11.4.1/v0.9',
       },
     },
   },
 };
+
+export default meta;
 
