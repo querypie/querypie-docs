69e692a6b1dd1b74251ff616cfb7f67e7176b48e
JK <jk@chequer.io>
2025-12-29 23:16:14 +0900

    Fix api-reference/_meta.ts
    - Do not use type: 'menu', because 'menu' is for top navbar.
    - No items are supported

--- src/content/ko/api-reference/_meta.ts
+++ src/content/ko/api-reference/_meta.ts
@@ -2,18 +2,7 @@ import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
   '11.4.1': {
-    type: 'menu',
-    title: '11.4.1',
-    items: {
-      v2: {
-        title: 'v2',
-        href: '11.4.1/v2',
-      },
-      'v0.9': {
-        title: 'v0.9',
-        href: '11.4.1/v0.9',
-      },
-    },
+    title: 'Version 11.4.1',
   },
 };
 
