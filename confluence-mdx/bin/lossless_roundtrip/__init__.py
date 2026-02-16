a521e0ac350c7bee7cf12718c9ed35dfb6e631e3
JK <jk@chequer.io>
2026-02-17 04:01:31 +0900

    feat(lossless): R2 rehydrator 최소 구현 추가

--- confluence-mdx/bin/lossless_roundtrip/__init__.py
+++ confluence-mdx/bin/lossless_roundtrip/__init__.py
@@ -7,11 +7,19 @@ from .sidecar import (
     load_sidecar,
     write_sidecar,
 )
+from .rehydrator import (
+    rehydrate_xhtml,
+    rehydrate_xhtml_from_files,
+    sidecar_matches_mdx,
+)
 
 __all__ = [
     "ROUNDTRIP_SCHEMA_VERSION",
     "RoundtripSidecar",
     "build_sidecar",
     "load_sidecar",
+    "rehydrate_xhtml",
+    "rehydrate_xhtml_from_files",
+    "sidecar_matches_mdx",
     "write_sidecar",
 ]
